1c71b603b664d07d9f4e88115ad72d309b6b139e
Ilya Savchuk <ilyas.sporichev@gmail.com>
2019-02-24 00:25:42 +0300

    test fixed

--- src/app/home/directives/cource-border.directive.spec.ts
+++ src/app/home/directives/cource-border.directive.spec.ts
@@ -2,6 +2,7 @@ import { CourceBorderDirective } from './cource-border.directive';
 
 describe('CourceBorderDirective', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     const directive = new CourceBorderDirective();
     expect(directive).toBeTruthy();
   });
